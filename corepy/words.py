60da633ffe91b2fe365f975de74876421a7c05a9
Daniel Archer <darcher@athene.bm>
2022-04-07 12:45:29 -0300

    update url with one that isn't blocked by webfilter

--- corepy/words.py
+++ corepy/words.py
@@ -1,6 +1,6 @@
 from urllib.request import urlopen
 
-story = urlopen('http://sixty-north.com/c/t.txt')
+story = urlopen('http://www.weather.bm/forecastpublicextended.asp')
 story_words = []
 for line in story:
     line_words = line.decode('utf-8').split()
